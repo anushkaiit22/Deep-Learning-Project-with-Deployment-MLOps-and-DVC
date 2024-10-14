ae32bb3b2244800ff4a073121ac98d9f545438a7
anushkaiit22 <pimpaleanushka1309@gmail.com>
2024-10-15 00:20:22 +0530

    Data Ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from Disease_Classifier import logger
+from Disease_Classifier.pipeline.stage1_di import DataIngestionTrainingPipeline
 
-logger.info("Lets start our log")
+STAGE_NAME = "Data Ingestion Stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
