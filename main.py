797128e1ad865d01bd2aa9a3f172741a06175484
anushkaiit22 <pimpaleanushka1309@gmail.com>
2024-10-15 01:27:55 +0530

    model training added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Disease_Classifier import logger
 from Disease_Classifier.pipeline.stage1_di import DataIngestionTrainingPipeline
 from Disease_Classifier.pipeline.stage2_pb import PrepareBaseModelTrainingPipeline
-
+from Disease_Classifier.pipeline.stage3_t import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -25,3 +25,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e      
+
+
+STAGE_NAME = "Training"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = ModelTrainingPipeline()
+   obj.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
